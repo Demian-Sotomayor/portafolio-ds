324a61d3ec2b0e9a49f3822d4ef2f8ad62c2be10
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2024-03-14 20:06:48 -0300

    Terminando about y projects, al momento toda la página es 100% responsive

--- src/front/js/views/About.jsx
+++ src/front/js/views/About.jsx
@@ -1,12 +1,33 @@
 import "../../styles/About.css";
+import demianProfile from "../../img/demian.jpg";
 
 const About = () => {
   return (
-    <div className="container-fluid">
-        {/* <h1>About</h1> */}
-        
+    <div className="container-fluid container-about">
+      <h1 className="about-title">About</h1>
+      <div className="img-container">
+        <img
+          src={demianProfile}
+          alt="demian-profile"
+          className="demian-profile"
+        />
+      </div>
+      <div className="about-texts">
+        <p>
+        My path into programming began in early 2022, when I was just 17 years old. I started studying the fundamentals and delving into web design using HTML5 and CSS3. As I gained momentum, I dug deeper and continued with JavaScript.
+        </p>
+        <p>
+        Upon discovering my passion for this field, I continued studying and entered an intensive course to learn better and polish myself a little more.
+        </p>
+        <p>
+        I started from web design to exploring ethical hacking and cybersecurity as a hobby and along the way I also studied how to work with terminals and shell scripts.
+        </p>
+        <p>
+        Currently, I am expanding my experience by learning about cloud development with Google Cloud. I&#39;m excited to continue growing and learning!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- src/front/js/views/App.jsx
+++ src/front/js/views/App.jsx
@@ -1,21 +1,21 @@
 import "../../styles/App.css";
 import About from "./About";
 import Home from "./Home";
-import verMas from "../../../assets/ver-mas.svg"
+import verMas from "../../../assets/ver-mas.svg";
+import Projects from "./Projects";
 
 const App = () => {
-
-
   return (
-    <> 
-    <div className="menu">
+    <>
+      <div className="menu">
         <p className="cambiar-esp">Es</p>
         <img src={verMas} alt="ver-mas" className="ver-mas-menu" />
-    </div>
-    <div className="m-5">
-      <Home />
-      <About />
-    </div>
+      </div>
+      <div className="m-5">
+        <Home />
+        <About />
+        <Projects />
+      </div>
     </>
   );
 };
