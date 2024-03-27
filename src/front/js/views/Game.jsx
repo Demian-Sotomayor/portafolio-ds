774768204dbca57b7023d79167c35865fc1c4aa6
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2024-03-26 21:44:00 -0300

    Añadiendo btn para volver a inicio + terminando de hacer responsive lo que llevo de hud

--- src/front/js/views/Game.jsx
+++ src/front/js/views/Game.jsx
@@ -1,32 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../../styles/Game.css";
 import marcoMenu from "../../../assets/marco-menu.svg";
 import michi from "../../../assets/michi.svg";
-import estambreFijoNivel1 from "../../img/game/con-hilo/bola-estambre-verde.png"
-import estambreFijoNivel2 from "../../img/game/con-hilo/bola-estambre-azul.png"
-import estambreFijoNivel3 from "../../img/game/con-hilo/bola-estambre-morada.png"
-import estambreFijoNivel4 from "../../img/game/con-hilo/bola-estambre-naranja.png"
-import estambreFijoNivel5 from "../../img/game/con-hilo/bola-estambre-roja.png"
+import estambreFijoNivel1 from "../../img/game/con-hilo/bola-estambre-verde.png";
+import estambreFijoNivel2 from "../../img/game/con-hilo/bola-estambre-azul.png";
+import estambreFijoNivel3 from "../../img/game/con-hilo/bola-estambre-morada.png";
+import estambreFijoNivel4 from "../../img/game/con-hilo/bola-estambre-naranja.png";
+import estambreFijoNivel5 from "../../img/game/con-hilo/bola-estambre-roja.png";
+import { Link } from "react-router-dom";
 
 const Game = () => {
-
   // Declarando opacidad así por ahora para que no de error
   const [opacity, setOpacity] = useState("opacity-25");
 
-  return (
-    <div className='container-fluid'>
-        <img src={michi} alt="" className="michi-menu-game" />
-        <img src={marcoMenu} alt="" className="marco-menu-game" />
+  const [idioma, setIdioma] = useState(localStorage.getItem("idioma") || "eng");
 
-        <div className="estambres-recuperados">
-          <img src={estambreFijoNivel1} alt="" className={opacity} />
-          <img src={estambreFijoNivel2} alt="" className={opacity} />
-          <img src={estambreFijoNivel3} alt="" className={opacity} />
-          <img src={estambreFijoNivel4} alt="" className={opacity} />
-          <img src={estambreFijoNivel5} alt="" className={opacity} />
-        </div>
+  return (
+    <div className="container-fluid">
+      <Link to="/" className="button-back-home">
+        <i class="fa-solid fa-arrow-left me-2"></i>
+        {idioma === "esp" ? "Inicio" : "Home"}
+      </Link>
+      <img src={michi} alt="" className="michi-menu-game" />
+      <img src={marcoMenu} alt="" className="marco-menu-game" />
+
+      <div className="estambres-recuperados">
+        <img src={estambreFijoNivel1} alt="" className={opacity} />
+        <img src={estambreFijoNivel2} alt="" className={opacity} />
+        <img src={estambreFijoNivel3} alt="" className={opacity} />
+        <img src={estambreFijoNivel4} alt="" className={opacity} />
+        <img src={estambreFijoNivel5} alt="" className={opacity} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Game;
