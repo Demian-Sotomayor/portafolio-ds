83652ede34db1e785868186fc5c80a0801b9fe81
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2024-03-27 12:31:19 -0300

    Intentando implementar lógica de juego, quizá toque aprender y usar canvas por problema de muchos render XD

--- src/front/js/views/Game.jsx
+++ src/front/js/views/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/Game.css";
 import marcoMenu from "../../../assets/marco-menu.svg";
 import michi from "../../../assets/michi.svg";
@@ -10,10 +10,63 @@ import estambreFijoNivel5 from "../../img/game/con-hilo/bola-estambre-roja.png";
 import { Link } from "react-router-dom";
 
 const Game = () => {
-  // Declarando opacidad así por ahora para que no de error
-  const [opacity, setOpacity] = useState("opacity-25");
-
+  // Cambiar idioma a inglés o español
   const [idioma, setIdioma] = useState(localStorage.getItem("idioma") || "eng");
+  // Temporalmente manejando la opacidad así
+  const [opacity, setOpacity] = useState(25);
+  // Bolas de estambre
+  const [balls, setBalls] = useState([]);
+  // Niveles
+  const [level, setLevel] = useState(1);
+  // Juego finalizado
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    initializeBalls();
+  }, []);
+
+  useEffect(() => {
+    if (gameOver) {
+      alert("¡Has recuperado todos los juguetes del gatito! ¡Victoria!")
+    }
+  }, [gameOver]);
+
+  const initializeBalls = () => {
+    // Definir arreglo con bolas de estambre y sus propiedades
+    const initialBalls = [
+      { id: 1, level: 1, recovered: false },      
+      { id: 2, level: 2, recovered: false },
+      { id: 3, level: 3, recovered: false },
+      { id: 4, level: 4, recovered: false },
+      { id: 5, level: 5, recovered: false },      
+    ]
+    setBalls(initialBalls);
+  } 
+
+  const handleBallClick = (ballId) => {
+    const updatedBalls = balls.map(ball => {
+      if(ball.id === ballId) {
+        return { ...ball, recovered: true }; // Marcar la bola como recuperada si coincide con el ID
+      }
+      return ball
+    });
+
+    setBalls(updatedBalls);
+
+    // Verificar si recuperamos todos los juguetes
+    const allRecovered = updatedBalls.every(ball => ball.recovered);
+    if(allRecovered) {
+      setGameOver(true);
+    }
+
+  }
+
+  const updateGame = () => {
+
+  }
+
+
+
 
   return (
     <div className="container-fluid">
@@ -25,11 +78,11 @@ const Game = () => {
       <img src={marcoMenu} alt="" className="marco-menu-game" />
 
       <div className="estambres-recuperados">
-        <img src={estambreFijoNivel1} alt="" className={opacity} />
-        <img src={estambreFijoNivel2} alt="" className={opacity} />
-        <img src={estambreFijoNivel3} alt="" className={opacity} />
-        <img src={estambreFijoNivel4} alt="" className={opacity} />
-        <img src={estambreFijoNivel5} alt="" className={opacity} />
+        <img src={estambreFijoNivel1} alt="" className={opacity >= 25 ? "opacity-25" : "opacity-75"} onClick={handleBallClick(1)} />
+        <img src={estambreFijoNivel2} alt="" className={opacity >= 25 ? "opacity-25" : "opacity-75"} onClick={handleBallClick(2)} />
+        <img src={estambreFijoNivel3} alt="" className={opacity >= 25 ? "opacity-25" : "opacity-75"} onClick={handleBallClick(3)} />
+        <img src={estambreFijoNivel4} alt="" className={opacity >= 25 ? "opacity-25" : "opacity-75"} onClick={handleBallClick(4)} />
+        <img src={estambreFijoNivel5} alt="" className={opacity >= 25 ? "opacity-25" : "opacity-75"} onClick={handleBallClick(5)} />
       </div>
     </div>
   );
