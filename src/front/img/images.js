f6ae3ee3dd9822316bbbeb21948457242c26d26d
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2024-04-15 19:57:45 -0400

    Añadiendo imagenes a cloudinary y consumiendolas para optimizar página

---
+++ src/front/img/images.js
@@ -0,0 +1,58 @@
+export const projectsImages = {
+    elRinconDelVino: {
+        1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219678/projects-portfolio/elRinconDelVino/u6pwrpasolpqojkcumzv.jpg",
+        2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219678/projects-portfolio/elRinconDelVino/pomyinyccywtnl4mcwi7.jpg",
+        3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219678/projects-portfolio/elRinconDelVino/wzdiimshgpcjccrxqkfy.jpg",
+        4: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219678/projects-portfolio/elRinconDelVino/byiqpopv638hu3uwin4f.jpg",
+        5: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219679/projects-portfolio/elRinconDelVino/tvls0qxxlvcbmznfewfc.jpg",
+        6: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219679/projects-portfolio/elRinconDelVino/pvsbdf05bvlpqyvehwsw.jpg",
+    },
+
+    battleship: {
+        1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219700/projects-portfolio/battleship/vdfpfzoj3sbmf0rhwg8e.jpg",
+        2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219700/projects-portfolio/battleship/ndgiev0w93fs1aidawsd.jpg",
+        3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219700/projects-portfolio/battleship/oy07tujlb52nej7mtrug.jpg",
+    },
+
+    starwars: {
+        1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219655/projects-portfolio/starwarsBlog/xxqtt2qixojlsqhxeqbw.jpg",
+        2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219655/projects-portfolio/starwarsBlog/ptzehhievtghpoihqiqp.jpg",
+        3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713219656/projects-portfolio/starwarsBlog/nz1f9c0yvxklnrmh1y5b.jpg",
+    },
+
+    michiClick: {
+        michiEng: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223719/projects-portfolio/oxd4yfmvjwfovmdpxabf.png",
+        michiEsp: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223719/projects-portfolio/lihl6yppnwyuolhejmgp.png",
+    },
+
+    profile: {
+        1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713225386/projects-portfolio/jdwk12yoo4z7haxagsww.jpg",
+    }
+}
+
+export const gameImages = {
+    juguetesFijos: {
+        estambreFijoNivel1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223687/game-portfolio/juguete-fijo/ieijafbwpv49tfe1t6yn.png",
+        estambreFijoNivel2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223687/game-portfolio/juguete-fijo/d23omudbfjer24iylcgh.png",
+        estambreFijoNivel3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223687/game-portfolio/juguete-fijo/vny9ucbjq16asf6kltcn.png",
+        estambreFijoNivel4: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223687/game-portfolio/juguete-fijo/zqc1hhrnkcrjardtvm5l.png",
+        estambreFijoNivel5: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223687/game-portfolio/juguete-fijo/vsth9dhpgqawwzaohvvv.png",
+    },
+
+    juguetesMovimiento: {
+        estambreNivel1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713224710/game-portfolio/juguete-movimiento/ihe4jaio5jsel3zthmy7.png",
+        estambreNivel2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713224710/game-portfolio/juguete-movimiento/uukha3ygyhaldpzlm8tb.png",
+        estambreNivel3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713224710/game-portfolio/juguete-movimiento/ebdldzw5xzktxgvqv6yk.png",
+        estambreNivel4: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713224712/game-portfolio/juguete-movimiento/yqez3nnhf02tib13jluz.png",
+        estambreNivel5: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713224710/game-portfolio/juguete-movimiento/rpdgkntvbq3bnadmytc2.png",
+    },
+
+    pescados: {
+        pescadoNivel1: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223703/game-portfolio/pescados/hv0vjennpxrrhbsgruzn.png",
+        pescadoNivel2: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223703/game-portfolio/pescados/xdhpzmvoxd1payrorrza.png",
+        pescadoNivel3: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223703/game-portfolio/pescados/payq2hpmusdcduy93y9e.png",
+        pescadoNivel4: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223703/game-portfolio/pescados/gcex15nvkuvnf0ji74q9.png",
+        pescadoNivel5: "https://res.cloudinary.com/db7vyckvo/image/upload/v1713223703/game-portfolio/pescados/yceu9k1iryuf8iwt8nwc.png",
+    }
+}
+
--- src/front/js/views/AllProjects.jsx
+++ src/front/js/views/AllProjects.jsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
 import Menu from "../components/Menu";
 import { useState } from "react";
-import "../../styles/AllProjects.css"
-import bt from "../../img/projects/battleship/1-bt.jpeg"
-import bt2 from "../../img/projects/battleship/2-bt.jpeg"
-import bt3 from "../../img/projects/battleship/3-bt.jpeg"
-import rdv from "../../img/projects/elRinconDelVino/1.jpeg"
-import rdv2 from "../../img/projects/elRinconDelVino/2.jpeg"
-import rdv3 from "../../img/projects/elRinconDelVino/3.jpeg"
-import sw from "../../img/projects/star-wars/1-sw.jpg"
-import sw2 from "../../img/projects/star-wars/2-sw.jpg"
-import sw3 from "../../img/projects/star-wars/3-sw.jpg"
+import "../../styles/AllProjects.css";
+import { projectsImages } from "../../img/images.js";
 
 const AllProjects = () => {
   const [isActive, setIsActive] = useState(false);
   const [idioma, setIdioma] = useState(localStorage.getItem("idioma") || "eng");
+  const [previewImage, setPreviewImage] = useState("");
+  const [currentProject, setCurrentProject] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [galleryImages, setGalleryImages] = useState([]);
+
+  const handleProjectHover = (projectName) => {
+    setCurrentProject(projectName);
+    setPreviewImage(projectsImages[projectName][1]);
+  };
+
+  const handlePreview = (projectName) => {
+    setCurrentProject(projectName);
+    setPreviewImage(projectsImages[projectName][1]);
+  };
+
+  const handleProjectClick = (projectName) => {
+    setCurrentProject(projectName);
+    setGalleryImages(Object.values(projectsImages[projectName]));
+    setShowModal(true);
+  };
 
   return (
-  
     <>
       <Menu
         setIsActive={setIsActive}
@@ -32,18 +43,68 @@ const AllProjects = () => {
 
       <div className="all-projects-container row">
         <div className="projects-left col-7">
-          <img src="" alt="" className="projects-images" />
+          <img src={previewImage} alt="" className="projects-images" />
         </div>
 
         <div className="projects-right col-5">
           <div className="projects-texts">
-            <h2 className="my-5" id="project1">- El Rincón del Vino</h2>
-            <h2 className="my-5" id="project2">- Battleship</h2>
-            <h2 className="my-5" id="project3">- Starwars Blog</h2>
+            <h2
+              className="my-5"
+              id="project1"
+              onMouseEnter={() => handleProjectHover("elRinconDelVino")}
+              onClick={handleProjectClick}
+            >
+              - El Rincón del Vino
+              <h6 className="opacity-50">
+                (Lamentablemente no cuento con imágenes de mejor resolución,
+                ¡Perdón!)
+              </h6>
+            </h2>
+            <h2
+              className="my-5"
+              id="project2"
+              onMouseEnter={() => handleProjectHover("battleship")}
+              onClick={handleProjectClick}
+            >
+              - Battleship
+            </h2>
+            <h2
+              className="my-5"
+              id="project3"
+              onMouseEnter={() => handleProjectHover("starwars")}
+              onClick={handleProjectClick}
+            >
+              - Starwars Blog
+            </h2>
             <h2 className="my-5">- ¡Más próximamente!</h2>
           </div>
         </div>
       </div>
+
+      {/* --- // MODAL // --- */}
+      {showModal && (
+        <div className="modal" tabIndex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{currentProject}</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setShowModal(false)}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="gallery">
+                  {galleryImages.map((image, index) => (
+                    <img key={index} src={image} alt={`Image ${index}`} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
